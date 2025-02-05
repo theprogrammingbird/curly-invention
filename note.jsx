82269b6ffbc89ec8112798578265119427f33761
theprogrammingbird <149425924+theprogrammingbird@users.noreply.github.com>
2025-02-05 19:53:00 +0100

    Create note.jsx

---
+++ note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Note = ({ note }) => {
+    return (
+        <div className="note">
+            <p>{note}</p>
+        </div>
+    );
+};
+
+export default Note;
